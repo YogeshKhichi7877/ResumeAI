9147fd2c414d5e14bbfd4993d87d153b539bab7e
Yogesh <ykhinchi38@gmail.com>
2026-03-20 18:48:15 +0530

    console.log() added

--- server/server.js
+++ server/server.js
@@ -35,7 +35,8 @@ app.use((req, res) => res.status(404).json({ message: 'Route not found' }));
 
 const PORT = process.env.PORT || 3014;
 app.listen(PORT, () => {
-  // Server started
+  console.log(`Connected to MongoDB`);
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
 module.exports = app;
