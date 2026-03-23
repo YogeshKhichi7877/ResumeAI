84fdc85aa309560da44a7008495f0b6666acfb40
Yogesh <ykhinchi38@gmail.com>
2026-03-23 11:31:32 +0530

    Cors updated

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization' ,  'admin-secret' , 'token' , 'X-Requested-With' , 'Accept' , 'Origin'],
 }));
 
 // Handle preflight requests
